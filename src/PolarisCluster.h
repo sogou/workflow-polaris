99674240a7da34f4ebede096cc8c0e47f91bef0e
xiehan <52160700+Barenboim@users.noreply.github.com>
2021-11-24 20:24:05 +0800

    remove inner_cluster_t and get_inner_cluster() interface.

--- src/PolarisCluster.h
+++ src/PolarisCluster.h
@@ -3,22 +3,13 @@
 
 namespace polaris {
 
-struct inner_cluster_t {
-    std::vector<std::string> server_connectors;
-    std::vector<std::string> discover_clusters;
-    std::vector<std::string> healthcheck_clusters;
-    std::vector<std::string> monitor_clusters;
-    std::vector<std::string> metrics_clusters;
-    std::map<std::string, std::string> revision_map;
-};
-
 class PolarisCluster {
   public:
     PolarisCluster() {
         this->ref = new std::atomic<int>(1);
         this->status = new int(0);
         this->mutex = new std::mutex;
-        this->inner_cluster = new inner_cluster_t;
+        this->data = new struct cluster_data;
     }
 
     virtual ~PolarisCluster() { this->decref(); }
@@ -27,7 +18,7 @@ class PolarisCluster {
         this->ref = copy.ref;
         this->mutex = copy.mutex;
         this->status = copy.status;
-        this->inner_cluster = copy.inner_cluster;
+        this->data = copy.data;
         this->incref();
     }
 
@@ -37,7 +28,7 @@ class PolarisCluster {
             this->ref = copy.ref;
             this->mutex = copy.mutex;
             this->status = copy.status;
-            this->inner_cluster = copy.inner_cluster;
+            this->data = copy.data;
             this->incref();
         }
         return *this;
@@ -45,9 +36,12 @@ class PolarisCluster {
 
     std::mutex *get_mutex() { return this->mutex; }
     int *get_status() { return this->status; }
-    struct inner_cluster_t *get_inner_cluster() {
-        return this->inner_cluster;
-    }
+    std::vector<std::string> *get_server_connectors() { return &this->data->server_connectors; }
+    std::vector<std::string> *get_discover_clusters() { return &this->data->discover_clusters; }
+    std::vector<std::string> *get_healthcheck_clusters() { return &this->data->healthcheck_clusters; }
+    std::vector<std::string> *get_monitor_clusters() { return &this->data->monitor_clusters; }
+    std::vector<std::string> *get_metrics_clusters() { return &this->data->metrics_clusters; }
+    std::map<std::string, std::string> *get_revision_map() { return &this->data->revision_map; }
 
   private:
     void incref() { (*this->ref)++; }
@@ -57,15 +51,26 @@ class PolarisCluster {
             delete this->ref;
             delete this->mutex;
             delete this->status;
-            delete this->inner_cluster;
+            delete this->data;
         }
     }
+
+  private:
+    struct cluster_data
+    {
+        std::vector<std::string> server_connectors;
+        std::vector<std::string> discover_clusters;
+        std::vector<std::string> healthcheck_clusters;
+        std::vector<std::string> monitor_clusters;
+        std::vector<std::string> metrics_clusters;
+        std::map<std::string, std::string> revision_map;
+    };
 
   private:
     std::atomic<int> *ref;
     int *status;
     std::mutex *mutex;
-    struct inner_cluster_t *inner_cluster;
+    struct cluster_data *data;
 };
 
 };  // namespace polaris
