ec67988a5f7c87f6ac484838e3cb3e81735c6249
holmes1412 <liyingxin1412@gmail.com>
2022-03-07 15:54:47 +0800

    update demo and dependencies path

--- example/demo.cc
+++ example/demo.cc
@@ -1,21 +1,16 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string>
-#include "src/PolarisManager.h"
+#include "PolarisManager.h"
 #include "workflow/WFFacilities.h"
-#include "workflow/WFHttpServer.h"
 
 #define RETRY_MAX 5
 #define REDIRECT_MAX 3
-#define INSTANCES_NUM 4
 
 using namespace polaris;
 
 static WFFacilities::WaitGroup main_wait_group(1);
 static WFFacilities::WaitGroup query_wait_group(1);
-static const std::string service_namespace = "default";
-static const std::string service_name = "workflow.polaris.service.b";
-WFHttpServer *instances[INSTANCES_NUM];
 
 void sig_handler(int signo)
 {
@@ -23,50 +18,21 @@ void sig_handler(int signo)
 	query_wait_group.done();
 }
 
-void start_test_servers()
+int main(int argc, char *argv[])
 {
-	for (size_t i = 0; i < INSTANCES_NUM; i++)
-	{
-		std::string port = "800";
-		port += std::to_string(i);
-
-		instances[i] = new WFHttpServer([port](WFHttpTask *task) {
-			task->get_resp()->append_output_body(
-						"Response from instance 127.0.0.0.1:" + port);
-		});
-
-		if (instances[i]->start(atoi(port.c_str())) != 0) {
-			fprintf(stderr, "Start instance 127.0.0.0.1:%s failed.\n",
-					port.c_str());
-			delete instances[i];
-			instances[i] = NULL;
-		}
-	}
-}
-
-void stop_test_servers()
-{
-	for (size_t i = 0; i < INSTANCES_NUM; i++)
-	{
-		if (instances[i])
-		{
-			instances[i]->stop();
-			delete instances[i];
-		}
-	}
-}
-
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "USAGE: %s <Polaris Cluster URL>\n", argv[0]);
+	if (argc != 4) {
+		fprintf(stderr, "USAGE: %s "
+				"<Polaris cluster URL> <service namespace> <service name>\n",
+				argv[0]);
 		exit(1);
 	}
 
 	signal(SIGINT, sig_handler);
 
-	start_test_servers();
-
 	std::string polaris_url = argv[1];
+	std::string service_namespace = argv[2];
+	std::string service_name = argv[3];
+
 	if (strncasecmp(argv[1], "http://", 7) != 0 &&
 		strncasecmp(argv[1], "https://", 8) != 0) {
 		polaris_url = "http://" + polaris_url;
@@ -110,8 +76,7 @@ int main(int argc, char *argv[]) {
 	fprintf(stderr, "\nUnwatch %s %s ret=%d.\n", service_namespace.c_str(),
 			service_name.c_str(), unwatch_ret);
 
-	stop_test_servers();
-	fprintf(stderr, "Success. Make sure the timer ends and press Ctrl-C to exit.\n");
+	fprintf(stderr, "Success. Make sure timer ends and press Ctrl-C to exit.\n");
 	main_wait_group.wait();
 
 	return 0;
